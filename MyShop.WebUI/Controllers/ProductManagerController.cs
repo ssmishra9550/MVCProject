b9bbfa46d02d78a3d64b078e02ce4b4228fbd266
ssmishra9550 <sauravmishra8333@gmail.com>
2022-06-30 10:47:54 +0530

    first commit

---
+++ MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductRepository context;
+        public ProductManagerController()
+        {
+            context = new ProductRepository();
+        }
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+    }
+}
